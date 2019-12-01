c8403eb7d4dc768731fb3ed83367e117909768da
Daniel Jay Haskin <djhaskin987@gmail.com>
2019-11-30 17:03:31 -0700

    Fix project and README

--- README.md
+++ README.md
@@ -2,18 +2,36 @@
 
 A Clojure library that makes CLI very nice, easy, and clojure-y.
 
+## Usage
+
+Leiningen:
+```
+[onecli 0.1.0]
+```
+
+Usage looks like this:
+
+```
+(ns my-program
+  (:require
+    [onecli.core :as onecli])
+  (:gen-class))
+
+(defn -main [& args]
+  (onecli/go!
+    {:program-name "my-program"
+     :functions {
+      [] main-cli!
+      ["do"] do-it!
+      ["do" "list"] do-list-it!
+     :env-vars (System/getenv)
+     :args args})
+)
+```
 
 ## License
 
-Copyright © 2019 Daniel Jay Haskin
+Copyright © 2017 Daniel Jay Haskin et. al., see the AUTHORS.md file.
 
-This program and the accompanying materials are made available under the
-terms of the Eclipse Public License 2.0 which is available at
-http://www.eclipse.org/legal/epl-2.0.
-
-This Source Code may also be made available under the following Secondary
-Licenses when the conditions for such availability set forth in the Eclipse
-Public License, v. 2.0 are satisfied: GNU General Public License as published
-by the Free Software Foundation, either version 2 of the License, or (at your
-option) any later version, with the GNU Classpath Exception which is available
-at https://www.gnu.org/software/classpath/license.html.
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
--- project.clj
+++ project.clj
@@ -1,19 +1,16 @@
-(defproject onecli "0.1.0-SNAPSHOT"
+(defproject onecli "0.1.0"
             :description "One CLI, for all people, for all time."
             :url "https://git.sr.ht/~djhaskin987/onecli"
-            :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-                      :url "https://www.eclipse.org/legal/epl-2.0/"}
+            :license {:name "Eclipse Public License"
+                      :url "http://www.eclipse.org/legal/epl-v10.html"}
             :dependencies [
                            [org.clojure/clojure "1.10.0"]
                            [cheshire "5.9.0"]
                            [clj-http "3.10.0"]
                            ]
-            :main onecli.cli
+            ;;:main onecli.cli
             :global-vars {*warn-on-reflection* true}
-            :plugins [[lein-licenses "0.2.2"]
-                      [lein-print "0.1.0"]]
             :repl-options {:init-ns onecli.core}
-
             :profiles {
                        :dev {
                              :dependencies [
@@ -22,11 +19,18 @@
                                             [cheshire "5.9.0"]
                                             [clj-http "3.10.0"]
                                             ]
-                             :plugins [[test2junit "1.3.3"]]
+                             :plugins [
+                                       [test2junit "1.3.3"]
+                                       [lein-licenses "0.2.2"]
+                                       [lein-print "0.1.0"]
+                                       ]
                              :test2junit-output-dir "target/test-results"
                              :injections [(require 'pjstadig.humane-test-output)
                                           (pjstadig.humane-test-output/activate!)]
                              }
+                       :jar {:aot [
+                                   onecli.core
+                                   ]}
                        :uberjar {:aot [
                                        onecli.core
                                        onecli.cli
